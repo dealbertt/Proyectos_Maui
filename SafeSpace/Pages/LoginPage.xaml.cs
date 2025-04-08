a1c7b0bdbb2039a5041cfe9e7ca947a06e55c7dc
dealbertt <dealbertignacio@gmail.com>
2025-04-08 13:29:34 +0200

    Animación de carga metida

--- SafeSpace/Pages/LoginPage.xaml.cs
+++ SafeSpace/Pages/LoginPage.xaml.cs
@@ -9,6 +9,18 @@ namespace SafeSpace.Pages
             InitializeComponent();
             BindingContext = new LoginPageViewModel();
         }
+        private async void OnLoadClicked(object sender, EventArgs e)
+        {
+            LoadingIndicator.IsRunning = true;
+            LoadingIndicator.IsVisible = true;
+
+
+            // Simulate some loading
+            await Task.Delay(3000); // Replace with your actual loading logic
+
+            LoadingIndicator.IsRunning = false;
+            LoadingIndicator.IsVisible = false;
+        }
 
     }
 }
